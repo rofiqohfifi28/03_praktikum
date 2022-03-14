75c5a39d81451c58c9be0de5d25a9eabf43774ee
fifirofiqoh28 <rofiqohwahyuningtyas@gmail.com>
2022-03-14 21:56:06 +0700

    praktikum 3

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main', ['title'=>'Contact']) 
+@section('container')
+<div class="pageTitle">
+	<div class="container">
+	<h2>Contact Us</h2>
+	</div>
+	</div>
+	
+	 <div class="container">
+	 <div class="bodyInner">
+			<p>
+			&nbsp;
+			</p>
+			<p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p><p>&nbsp;
+			</p>
+	</div>
+	</div>
+  @endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="themes/assets/ico/favicon.ico">
+    <title>{{ $title }}</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="themes/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="themes/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="themes/assets/css/carousel.css" rel="stylesheet">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    @include('layouts.navbar')
+
+
+
+	<div class="pageTitle">
+	@yield('container')
+	
+
+	<div class="highlightSection">
+	<div class="container">
+	<div class="row">
+		<div class="col-lg-4">
+		<div class="media">
+			<a href="menu/"><img src="http://dev.bombaytakeawayclub.com/assets/img/nepali-momo.png" alt="nepali-momo"> </a>
+			<h3 class="media-heading text-primary-theme">NEPALESE LAMB MOMO</h3>
+			<p>Steamed dumplings filled with slightly spiced minced meat served with special sauce.</p>
+		</div>
+		</div>
+		<div class="col-lg-4">
+		<div class="media"><a href="menu/"><img src="http://dev.bombaytakeawayclub.com/assets/img/gorkha-special-chicken.png" alt="GURKHA SPECIAL CHICKEN"> </a>						
+		<h3 class="media-heading text-danger-theme">GURKHA SPECIAL CHICKEN</h3>
+		<p>Boneless chicken marinated in mustard, smoked chilli, herbs and spices slowly cooked in tandoor. </p>
+		
+		</div>
+		</div>
+		<div class="col-lg-4">
+		<div class="media">
+		<a href="menu/"><img src="http://dev.bombaytakeawayclub.com/assets/img/lam-tikka.png" alt="Lam Tikka"> </a>
+		<h3 class="media-heading">LAMB TIKKA SPECIAL</h3>
+		<p>Tender pieces of lamb mixed with our own spices and gently cooked in clay oven. </p>
+		</div>
+		</div>
+	</div>
+	</div>
+	</div>
+	
+	
+
+	<div class="introSection">
+		<div class="container">
+		<div class="row">
+		<div class="col-lg-5">
+		<h3>Welcome to restaurent</h3>
+		<p>
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+		text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+		It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <br><br>
+		It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+		and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		</p>
+		</div>
+		
+		<div class="col-lg-4">
+		<h3>Welcome to restaurent</h3>
+		<p>
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+		text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+		It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. <br><br>
+		
+		</p>
+		
+		</div>
+		
+		<div class="col-lg-3">
+		<h3>Welcome to restaurent</h3>
+		<p>
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy 
+		text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+		
+		</p>
+		
+		</div>
+		
+		</div>
+		</div>
+	</div>
+
+
+      <!-- FOOTER -->
+      @include('layouts.footer')
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="themes/dist/js/bootstrap.min.js"></script>
+    <script src="themes/assets/js/holder.js"></script>
+  </body>
+</html>
+
